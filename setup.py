6f55812af5d1af9da032725a54cfd401855074d4
Churitoring <dlwhdgus665@gmail.com>
2024-02-08 23:31:27 +0900

    버전 변경

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name="Program Updater",
-    version="0.0.2",
+    version="1.0.3",
     description="Program Updater By Churitoring",
     executables=[Executable("programupdater.py", icon="programupdater.ico")]
 )

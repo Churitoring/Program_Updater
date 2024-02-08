ec20c04b063f721c5300a72a95a0f423e61ded35
Churitoring <dlwhdgus665@gmail.com>
2024-02-08 23:10:36 +0900

    json 순서 변경 및 Delta 업데이트 추가

--- jsongen.py
+++ jsongen.py
@@ -3,11 +3,12 @@ import json
 
 def save():
     data = {
-        "executable_name": executable_name_entry.get(),
-        "program_name": program_name_entry.get(),
         "github_repo": github_repo_entry.get(),
+        "program_name": program_name_entry.get(),
+        "executable_name": executable_name_entry.get(),
         "folder_remove": folder_remove_var.get(),
-        "delete_zerobyte": delete_zerobyte_var.get()
+        "delete_zerobyte": delete_zerobyte_var.get(),
+        "delta_update": delta_update_var.get()
     }
     with open('management.json', 'w') as f:
         json.dump(data, f)
@@ -16,26 +17,30 @@ def save():
 root = tk.Tk()
 root.title("Configuration")
 
-tk.Label(root, text="Executable Name").grid(row=0)
+tk.Label(root, text="GitHub Repository").grid(row=0)
 tk.Label(root, text="Program Name").grid(row=1)
-tk.Label(root, text="GitHub Repository").grid(row=2)
+tk.Label(root, text="Executable Name").grid(row=2)
 tk.Label(root, text="Remove Folder").grid(row=3)
 tk.Label(root, text="Delete Zerobyte").grid(row=4)
+tk.Label(root, text="Delta Update").grid(row=5)
 
-executable_name_entry = tk.Entry(root)
-program_name_entry = tk.Entry(root)
 github_repo_entry = tk.Entry(root)
+program_name_entry = tk.Entry(root)
+executable_name_entry = tk.Entry(root)
 folder_remove_var = tk.BooleanVar()
 folder_remove_checkbutton = tk.Checkbutton(root, variable=folder_remove_var)
 delete_zerobyte_var = tk.BooleanVar()
 delete_zerobyte_checkbutton = tk.Checkbutton(root, variable=delete_zerobyte_var)
+delta_update_var = tk.BooleanVar()
+delta_update_checkbutton = tk.Checkbutton(root, variable=delta_update_var)
 
-executable_name_entry.grid(row=0, column=1)
+github_repo_entry.grid(row=0, column=1)
 program_name_entry.grid(row=1, column=1)
-github_repo_entry.grid(row=2, column=1)
+executable_name_entry.grid(row=2, column=1)
 folder_remove_checkbutton.grid(row=3, column=1)
 delete_zerobyte_checkbutton.grid(row=4, column=1)
+delta_update_checkbutton.grid(row=5, column=1)
 
-tk.Button(root, text='Save', command=save).grid(row=5, column=0, sticky=tk.W, pady=4)
+tk.Button(root, text='Save', command=save).grid(row=6, column=0, sticky=tk.W, pady=4)
 
 tk.mainloop()
--- programupdaterupdater.py
+++ programupdaterupdater.py
@@ -7,17 +7,6 @@ import time
 import threading
 import sys
 
-executable_name = ""
-program_name = "This Program Updater"
-github_repo = "Churitoring/Program_Updater"
-folder_remove = False
-delete_zerobyte = False
-
-if github_repo.startswith('/'):
-    github_repo = github_repo[1:]
-if github_repo.endswith('/'):
-    github_repo = github_repo[:-1]
-
 def loading_spinner():
     spinner = ['\\', '|', '/', '-']
     i = 0
@@ -25,6 +14,109 @@ def loading_spinner():
         print(f"\r업데이트중! {spinner[i % len(spinner)]}", end="")
         i += 1
         time.sleep(0.1)
+
+def print_update_status(local_version, target_version):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if local_version == '':
+        print(f"이 창을 닫지 마세요. \n{target_version}까지 최초 다운로드 중...")
+    else:
+        print(f"이 창을 닫지 마세요. \n{local_version} → {target_version}")
+
+def create_and_execute_updating_file(updating_file_path, update_file_path, updatingexe_file_path):
+    if not os.path.exists(updating_file_path):
+        shutil.copy(update_file_path, updatingexe_file_path)
+        open(updating_file_path, 'w').close()
+        Popen([updatingexe_file_path])
+        os._exit(0)
+    else:
+        os.remove(updating_file_path)
+
+def download_and_extract_assets(assets, target_version):
+    for asset in assets:
+        file_url = asset['browser_download_url']
+        file_name = os.path.join(current_directory, asset['name'])
+
+        try:
+            file_response = requests.get(file_url)
+            file_response.raise_for_status()
+
+            with open(file_name, 'wb') as f:
+                f.write(file_response.content)
+
+            if file_name.endswith('.zip'):
+                with zipfile.ZipFile(file_name, 'r') as zip_ref:
+                    if folder_remove == True:
+                        for member in zip_ref.infolist():
+                            try:
+                                member.filename = member.filename.encode('cp437').decode('euc-kr', 'ignore')
+                            except UnicodeDecodeError:
+                                member.filename = member.filename.encode('cp437').decode('utf-8', 'ignore')
+                            # 최상위 폴더 구조를 무시하고 파일 경로에서 상위 폴더명을 제거
+                            relative_path = os.path.relpath(member.filename, member.filename.split('/', 1)[0])
+                            target_path = os.path.join(current_directory, relative_path)
+                            # 폴더인 경우 폴더를 생성
+                            if member.is_dir():
+                                os.makedirs(target_path, exist_ok=True)
+                            else:  # 파일인 경우 파일을 쓴다.
+                                try:
+                                    with zip_ref.open(member) as source:
+                                        with open(target_path, 'wb') as target:
+                                            shutil.copyfileobj(source, target)
+                                except Exception as e:
+                                    continue  # 덮어쓰기 실패하면 무시하고 계속 진행
+                    else:
+                        for member in zip_ref.infolist():
+                            try:
+                                zip_ref.extract(member, current_directory)
+                            except Exception as e:
+                                continue  # 덮어쓰기 실패하면 무시하고 계속 진행
+
+            # 압축 파일 삭제
+            try:
+                os.remove(file_name)
+            except Exception as e:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"압축 파일 삭제 중 오류가 발생했습니다: {e}")
+
+        except requests.exceptions.RequestException as e:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"파일 다운로드 중 오류가 발생했습니다: {e}")
+
+    with open(version_file_path, 'w', encoding='utf-8') as version_file:
+        version_file.write(target_version)
+
+    Popen([update_file_path])
+    os._exit(0)
+
+def delete_empty_files(directory):
+    for foldername, _, filenames in os.walk(directory, topdown=False):
+        for filename in filenames:
+            file_path = os.path.join(foldername, filename)
+            if os.path.getsize(file_path) == 0:
+                try:
+                    os.remove(file_path)
+                    print(f'{file_path} has been removed.')
+                except Exception as e:
+                    print(f'Failed to remove {file_path}: {str(e)}')
+        
+        if not os.listdir(foldername):
+            try:
+                os.rmdir(foldername)
+                print(f'Empty directory {foldername} has been removed.')
+            except Exception as e:
+                print(f'Failed to remove directory {foldername}: {str(e)}')
+
+github_repo = "Churitoring/Program_Updater"
+program_name = "This Program Updater"
+executable_name = ""
+folder_remove = False
+delete_zerobyte = False
+delta_update = False
+
+if github_repo.startswith('/'):
+    github_repo = github_repo[1:]
+if github_repo.endswith('/'):
+    github_repo = github_repo[:-1]
 
 spinner_thread = threading.Thread(target=loading_spinner)
 spinner_thread.daemon = True
@@ -47,110 +139,36 @@ else:
     with open(version_file_path, 'r', encoding='utf-8') as version_file:
         local_version = version_file.read().strip()
 
-try:
-    response = requests.get(f'https://api.github.com/repos/{github_repo}/releases/latest')
-    response.raise_for_status()
-    latest_version = response.json()['tag_name']
-except requests.exceptions.RequestException as e:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(f"최신 버전을 확인하는 중 오류가 발생했습니다: {e}")
-    latest_version = local_version
-
-if local_version != latest_version:
-    if not os.path.exists(updating_file_path):
-        shutil.copy(update_file_path, updatingexe_file_path)
-        open(updating_file_path, 'w').close()
-        Popen([updatingexe_file_path])
-        os._exit(0)
-    else:
+if delta_update == True:
+    try:
+        response = requests.get(f'https://api.github.com/repos/{github_repo}/releases')
+        response.raise_for_status()
+        releases = response.json()
+        # 버전을 기준으로 오름차순 정렬
+        releases.sort(key=lambda x: x['tag_name'])
+    except requests.exceptions.RequestException as e:
         os.system('cls' if os.name == 'nt' else 'clear')
-        os.remove(updating_file_path)
-        if local_version == '':
-            print(f"이 창을 닫지 마세요. \n{latest_version}까지 최초 다운로드 중...")
-        else:
-            print(f"이 창을 닫지 마세요. \n{local_version} → {latest_version}")
-
-        try:
-            response = requests.get(f'https://api.github.com/repos/{github_repo}/releases/latest')
-            response.raise_for_status()
-
-            for asset in response.json()['assets']:
-                file_url = asset['browser_download_url']
-                file_name = os.path.join(current_directory, asset['name'])
-
-                try:
-                    file_response = requests.get(file_url)
-                    file_response.raise_for_status()
-
-                    with open(file_name, 'wb') as f:
-                        f.write(file_response.content)
-
-                    if file_name.endswith('.zip'):
-                        with zipfile.ZipFile(file_name, 'r') as zip_ref:
-                            if folder_remove == True:
-                                for member in zip_ref.infolist():
-                                    try:
-                                        member.filename = member.filename.encode('cp437').decode('euc-kr', 'ignore')
-                                    except UnicodeDecodeError:
-                                        member.filename = member.filename.encode('cp437').decode('utf-8', 'ignore')
-                                    # 최상위 폴더 구조를 무시하고 파일 경로에서 상위 폴더명을 제거
-                                    relative_path = os.path.relpath(member.filename, member.filename.split('/', 1)[0])
-                                    target_path = os.path.join(current_directory, relative_path)
-                                    # 폴더인 경우 폴더를 생성
-                                    if member.is_dir():
-                                        os.makedirs(target_path, exist_ok=True)
-                                    else:  # 파일인 경우 파일을 쓴다.
-                                        try:
-                                            with zip_ref.open(member) as source:
-                                                with open(target_path, 'wb') as target:
-                                                    shutil.copyfileobj(source, target)
-                                        except Exception as e:
-                                            continue  # 덮어쓰기 실패하면 무시하고 계속 진행
-                            else:
-                                for member in zip_ref.infolist():
-                                    try:
-                                        zip_ref.extract(member, current_directory)
-                                    except Exception as e:
-                                        continue  # 덮어쓰기 실패하면 무시하고 계속 진행
-
-                    # 압축 파일 삭제
-                    try:
-                        os.remove(file_name)
-                    except Exception as e:
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        print(f"압축 파일 삭제 중 오류가 발생했습니다: {e}")
-
-                except requests.exceptions.RequestException as e:
-                    os.system('cls' if os.name == 'nt' else 'clear')
-                    print(f"파일 다운로드 중 오류가 발생했습니다: {e}")
-
-            with open(version_file_path, 'w', encoding='utf-8') as version_file:
-                version_file.write(latest_version)
-        except requests.exceptions.RequestException as e:
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print(f"최신 릴리즈 다운로드 중 오류가 발생했습니다: {e}")
-
-        Popen([update_file_path])
-        os._exit(0)
-
-def delete_empty_files(directory):
-    for foldername, _, filenames in os.walk(directory, topdown=False):
-        for filename in filenames:
-            file_path = os.path.join(foldername, filename)
-            if os.path.getsize(file_path) == 0:
-                try:
-                    os.remove(file_path)
-                    print(f'{file_path} has been removed.')
-                except Exception as e:
-                    print(f'Failed to remove {file_path}: {str(e)}')
-        
-        if not os.listdir(foldername):
-            try:
-                os.rmdir(foldername)
-                print(f'Empty directory {foldername} has been removed.')
-            except Exception as e:
-                print(f'Failed to remove directory {foldername}: {str(e)}')
-
+        print(f"릴리즈 목록을 가져오는 중 오류가 발생했습니다: {e}")
+        releases = []
+    for release in releases:
+        release_version = release['tag_name']
+        if release_version > local_version:
+            create_and_execute_updating_file(updating_file_path, update_file_path, updatingexe_file_path)
+            print_update_status(local_version, release_version)
+            download_and_extract_assets(release['assets'], release_version)
+else:
+    try:
+        response = requests.get(f'https://api.github.com/repos/{github_repo}/releases/latest')
+        response.raise_for_status()
+        latest_version = response.json()['tag_name']
+    except requests.exceptions.RequestException as e:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"최신 버전을 확인하는 중 오류가 발생했습니다: {e}")
+        latest_version = local_version
+    if local_version != latest_version:
+        create_and_execute_updating_file(updating_file_path, update_file_path, updatingexe_file_path)
+        print_update_status(local_version, latest_version)
+        download_and_extract_assets(response.json()['assets'], latest_version)
 
 if os.path.exists(updatingexe_file_path):
     os.remove(updatingexe_file_path)

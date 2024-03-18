6c634e9b7720689b6bf841d18e6badb7195dee90
Churitoring <dlwhdgus665@gmail.com>
2024-03-18 23:18:01 +0900

    빌더 수정

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@
 from cx_Freeze import setup, Executable 
 
 setup(
-    name="Program Updater",
-    version="1.0.4",
-    description="Program Updater By Churitoring",
     executables=[Executable("programupdater.py", icon="programupdater.ico")]
 )
